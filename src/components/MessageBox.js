eab47098dbcd07470a12395dab41036279dbcf49
Ayaan Farrukh <ayaanfarrukhraza@gmail.com>
2025-08-19 20:58:27 +0500

    fixed

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -29,7 +29,7 @@ const MessageBox = ({MessagesSeen,messages,index,msg,otherUser}) => {
     <div className={`flex items-center flex-row ${msg.sender === user._id ? "justify-end bg" : "justify-start" }`}>
       {msg.sender !== user._id ? (
         <>
-        <div className='w-fit max-w-[50%] h-fit shaow rounded-full mt-3 mr-3'>
+        <div className='w-fit max-w-[75%] h-fit shaow rounded-full mt-3 mr-3'>
         <img className='w-10 h-10 sm:w-10 sm:h-10 md:w-10 md:h-10 object-cover rounded-full shadow-sm' src={msg.sender === user._id ? user.profile_pic : otherUser.profile_pic}></img>
         </div>
         <div className={`shadow-md border mt-2 ${msg.sender === user._id ? "bg-green-300" : "bg-white" } text-gray-800 w-fit h-fit p-4 break-words  rounded-md`}>
@@ -40,7 +40,7 @@ const MessageBox = ({MessagesSeen,messages,index,msg,otherUser}) => {
         </>
       ) : (
         <>
-        <div className={`shadow-md border mt-2 ${msg.sender === user._id ? "bg-green-300" : "bg-white" } text-gray-800 w-fit h-fit p-4 break-words max-w-[50%]  rounded-md`}>
+        <div className={`shadow-md border mt-2 ${msg.sender === user._id ? "bg-green-300" : "bg-white" } text-gray-800 w-fit h-fit p-4 break-words max-w-[75%]  rounded-md`}>
           {msg.text}
           <div className="flex justify-end text-xs text-gray-600 mt-1">{formattedDate.split(" ")[1]} {formattedDate.split(" ")[0]}, {timeInPk}</div>
         </div>
